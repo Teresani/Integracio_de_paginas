0584021ccc9425822860efb75b5ec507da1230bc
Jordan Iralde <iraldejordan10@gmail.com>
2025-01-13 10:41:39 -0300

    Forma Correcta header footer

--- AngularPage/src/app/app.routes.ts
+++ AngularPage/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component'; 
 
 export const routes: Routes = [
     {path: '', component: HeaderComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'footer', component: FooterComponent}
 ];
